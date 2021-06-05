d051d1c03319d9b48dcf60150ad2faa62a9b2036
Harrison Roy <koalasoftsolution@gmail.com>
2021-06-05 18:50:27 +0600

    changes

--- new_inventory.sql
+++ new_inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2021 at 02:47 PM
+-- Generation Time: Jun 05, 2021 at 02:49 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.1
 

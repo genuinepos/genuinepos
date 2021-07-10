590c7b80b6a0c64de17770a119bae6b6fa84a04f
Harrison Roy <koalasoftsolution@gmail.com>
2021-07-10 10:59:02 +0600

    added some changes

--- app/Http/Controllers/GeneralSettingController.php
+++ app/Http/Controllers/GeneralSettingController.php
@@ -255,14 +255,22 @@ class GeneralSettingController extends Controller
 
     public function emailSettings(Request $request)
     {
-        Artisan::call("env:set MAIL_MAILER='" . $request->get('MAIL_MAILER') . "'");
-        Artisan::call("env:set MAIL_HOST='" . $request->get('MAIL_HOST') . "'");
-        Artisan::call("env:set MAIL_PORT='" . $request->get('MAIL_PORT') . "'");
-        Artisan::call("env:set MAIL_USERNAME='" . $request->get('MAIL_USERNAME') . "'");
-        Artisan::call("env:set MAIL_PASSWORD='" . $request->get('MAIL_PASSWORD') . "'");
-        Artisan::call("env:set MAIL_ENCRYPTION='" . $request->get('MAIL_ENCRYPTION') . "'");
-        Artisan::call("env:set MAIL_FROM_ADDRESS='" . $request->get('MAIL_FROM_ADDRESS') . "'");
-        Artisan::call("env:set MAIL_FROM_NAME='" . $request->get('MAIL_FROM_NAME') . "'");
+        $MAIL_FROM_NAME = str_replace('"','',$request->get('MAIL_FROM_NAME'));
+        $MAIL_FROM_ADDRESS = str_replace('"','',$request->get('MAIL_FROM_ADDRESS'));
+        $MAIL_ENCRYPTION = str_replace('"','',$request->get('MAIL_ENCRYPTION'));
+        $MAIL_PASSWORD = str_replace('"','',$request->get('MAIL_PASSWORD'));
+        $MAIL_USERNAME = str_replace('"','',$request->get('MAIL_USERNAME'));
+        $MAIL_PORT = str_replace('"','',$request->get('MAIL_PORT'));
+        $MAIL_HOST = str_replace('"','',$request->get('MAIL_HOST'));
+        $MAIL_MAILER = str_replace('"','',$request->get('MAIL_MAILER'));
+        Artisan::call("env:set MAIL_MAILER='" . $MAIL_MAILER . "'");
+        Artisan::call("env:set MAIL_HOST='" . $MAIL_HOST . "'");
+        Artisan::call("env:set MAIL_PORT='" . $MAIL_PORT . "'");
+        Artisan::call("env:set MAIL_USERNAME='" . $MAIL_USERNAME . "'");
+        Artisan::call("env:set MAIL_PASSWORD='" . $MAIL_PASSWORD . "'");
+        Artisan::call("env:set MAIL_ENCRYPTION='" . $MAIL_ENCRYPTION  . "'");
+        Artisan::call("env:set MAIL_FROM_ADDRESS='" . $MAIL_FROM_ADDRESS . "'");
+        Artisan::call("env:set MAIL_FROM_NAME='" . $MAIL_FROM_NAME . "'");
         return response()->json('Email settings updated successfully');
     }
 }

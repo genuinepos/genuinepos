ff68c121aa3e00a26a2211480df0d08fe7131b66
Harrison Roy <koalasoftsolution@gmail.com>
2021-08-24 12:09:12 +0600

    added some changes

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -928,7 +928,9 @@ class SaleController extends Controller
             json_decode($prefixSettings->send_es_settings, true)['send_inv_via_email'] == '1'
         ) {
             if ($customer && $customer->email) {
-                dispatch(new SaleMailJob($customer->email, $sale));
+                //dispatch(new SaleMailJob($customer->email, $sale))->delay(now()->addSeconds(5));
+                SaleMailJob::dispatch($customer->email, $sale)
+                    ->delay(now()->addSeconds(5));
             }
         }
 

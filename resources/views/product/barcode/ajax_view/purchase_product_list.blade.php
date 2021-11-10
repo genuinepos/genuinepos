29f7a7d48c506f34431323ee709d56c6cabcd126
Harrison Roy <koalasoftsolution@gmail.com>
2021-11-10 18:20:48 +0600

    ok

--- resources/views/product/barcode/ajax_view/purchase_product_list.blade.php
+++ resources/views/product/barcode/ajax_view/purchase_product_list.blade.php
@@ -4,7 +4,7 @@
             <th class="text-start"><input type="checkbox" id="chack_all">All</th>
             <th class="text-start">Product</th>
             <th class="text-start">Supplier</th>
-            <th class="text-end">Pending Qty</th>
+            <th class="text-end">Quantity</th>
         </tr>
     </thead>
     <tbody id="purchased_product_list">

6a35e0fe4b162bcccf9dff75e6b94ac810dbc36d
Harrison Roy <koalasoftsolution@gmail.com>
2021-11-11 19:17:22 +0600

    some changes

--- resources/views/product/products/ajax_view/partials/warehouse_stock_details.blade.php
+++ resources/views/product/products/ajax_view/partials/warehouse_stock_details.blade.php
@@ -6,9 +6,11 @@
             <th class="text-white text-start">Product</th>
             <th class="text-white text-start">Warehouse</th>
             <th class="text-white text-start">Current Stock</th>
-            <th class="text-white text-start">Stock Value</th>
-            <th class="text-white text-start">Total Transfered</th>
-            <th class="text-white text-start">Total Adjusted</th>
+            <th class="text-white text-start">Stock Value({{ json_decode($generalSettings->business, true)['currency'] }})</th>
+            <th class="text-white text-start">Total Purchased(+)</th>
+            <th class="text-white text-start">Total received(+)</th>
+            <th class="text-white text-start">Total Adjusted(-)</th>
+            <th class="text-white text-start">Total Transferred(-)</th>
         </tr>
     </thead>
     <tbody>
@@ -25,23 +27,27 @@
                         @endphp
                         {{ App\Utils\Converter::format_in_bdt($currentStockValue) }}
                     </td>
-                    <td class="text-start">0.00</td>
-                    <td class="text-start">0.00</td>
+                    <td class="text-start">{{ $row->v_total_purchased.'('.$product->unit->code_name.')' }}</td>
+                    <td class="text-start">{{ $row->v_total_received.'('.$product->unit->code_name.')' }}</td>
+                    <td class="text-start">{{ $row->v_total_adjusted.'('.$product->unit->code_name.')' }}</td>
+                    <td class="text-start">{{ $row->v_total_transferred.'('.$product->unit->code_name.')' }}</td>
                 </tr>
             @else 
                 <tr>
                     <td class="text-start">{{ $product->product_code }}</td>
                     <td class="text-start">{{ $product->name }}</td>
                     <td class="text-start">{{ $row->warehouse_name.'/'.$row->warehouse_code }}</td>
-                    <td class="text-start">{{ $row->product_quantity.'('.$product->unit->code_name.')' }}</td>
+                    <td class="text-start"><b>{{ $row->product_quantity.'('.$product->unit->code_name.')' }}</b></td>
                     <td class="text-start">
                         @php
                             $currentStockValue = $product->product_cost_with_tax * $row->product_quantity;
                         @endphp
                         {{ App\Utils\Converter::format_in_bdt($currentStockValue) }}
                     </td>
-                    <td class="text-start">0.00</td>
-                    <td class="text-start">0.00</td>
+                    <td class="text-start">{{ $row->total_purchased.'('.$product->unit->code_name.')' }}</td>
+                    <td class="text-start">{{ $row->total_received.'('.$product->unit->code_name.')' }}</td>
+                    <td class="text-start">{{ $row->total_adjusted.'('.$product->unit->code_name.')' }}</td>
+                    <td class="text-start">{{ $row->total_transferred.'('.$product->unit->code_name.')' }}</td>
                 </tr>
             @endif
         @endforeach

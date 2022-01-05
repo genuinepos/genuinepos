b0cf6da39e5a78c2f865fa9abb2c93bdaecb5c1e
Harrison Roy <koalasoftsolution@gmail.com>
2022-01-05 16:10:27 +0600

    SupplierController, UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,10 +28,10 @@ class UserController extends Controller
         if ($request->ajax()) {
             $users = '';
             $query = DB::table('admin_and_users')
-            ->leftJoin('branches', 'admin_and_users.branch_id', 'branches.id')
-            ->leftJoin('roles', 'admin_and_users.role_id', 'roles.id')
-            ->leftJoin('hrm_department', 'admin_and_users.department_id', 'hrm_department.id')
-            ->leftJoin('hrm_designations', 'admin_and_users.designation_id', 'hrm_designations.id');
+                ->leftJoin('branches', 'admin_and_users.branch_id', 'branches.id')
+                ->leftJoin('roles', 'admin_and_users.role_id', 'roles.id')
+                ->leftJoin('hrm_department', 'admin_and_users.department_id', 'hrm_department.id')
+                ->leftJoin('hrm_designations', 'admin_and_users.designation_id', 'hrm_designations.id');
 
             if ($request->branch_id) {
                 if ($request->branch_id == 'NULL') {
@@ -41,24 +41,19 @@ class UserController extends Controller
                 }
             }
 
+            $query->select(
+                'admin_and_users.*',
+                'branches.name as branch_name',
+                'branches.branch_code',
+                'roles.name as role_name',
+                'hrm_department.department_name as dep_name',
+                'hrm_designations.designation_name as des_name',
+            );
+
             if (auth()->user()->role_type == 1 || auth()->user()->role_type == 2) {
-                $users = $query->select(
-                    'admin_and_users.*',
-                    'branches.name as branch_name',
-                    'branches.branch_code',
-                    'roles.name as role_name',
-                    'hrm_department.department_name as dep_name',
-                    'hrm_designations.designation_name as des_name',
-                )->orderBy('id', 'desc');
+                $users = $query->orderBy('id', 'desc');
             } else {
-                $users = $query->select(
-                    'admin_and_users.*',
-                    'branches.name as branch_name',
-                    'branches.branch_code',
-                    'roles.name as role_name',
-                    'hrm_department.department_name as dep_name',
-                    'hrm_designations.designation_name as des_name',
-                )->where('admin_and_users.branch_id', auth()->user()->branch_id)
+                $users = $query->where('admin_and_users.branch_id', auth()->user()->branch_id)
                     ->orderBy('id', 'desc');
             }
 
@@ -82,22 +77,22 @@ class UserController extends Controller
                     }
                 })
                 ->editColumn('role_name',  function ($row) {
-                    if ($row->role_type == 1){
-                       return 'Super-Admin';
-                    }elseif($row->role_type == 2)   {
+                    if ($row->role_type == 1) {
+                        return 'Super-Admin';
+                    } elseif ($row->role_type == 2) {
                         return 'Admin';
-                    }elseif($row->role_type == 3){
+                    } elseif ($row->role_type == 3) {
                         return  $row->role_name;
-                    }else {
-                       return '<span class="badge bg-warning text-white">No Role</span>';
-                    }  
+                    } else {
+                        return '<span class="badge bg-warning text-white">No Role</span>';
+                    }
                 })
                 ->editColumn('username',  function ($row) {
-                    if ($row->username){
-                       return $row->username;
-                    }else {
+                    if ($row->username) {
+                        return $row->username;
+                    } else {
                         return '<span class="badge bg-secondary"><b>Login not allowed</b></span>';
-                    }  
+                    }
                 })
                 ->setRowClass('text-start')
                 ->rawColumns(['action', 'branch', 'role_name', 'username'])
@@ -128,7 +123,7 @@ class UserController extends Controller
             'email' => 'required|unique:admin_and_users,email',
         ]);
 
-    
+
 
         if (isset($request->allow_login)) {
             $this->validate($request, [
@@ -145,7 +140,7 @@ class UserController extends Controller
                 'emp_id' => 'required',
                 'salary' => 'required',
                 'pay_type' => 'required',
-            ],[
+            ], [
                 'department_id.required' => 'Department field is required.',
                 'shift_id.required' => 'Shift field is required.',
                 'emp_id.required' => 'Employee ID field is required.',
@@ -175,7 +170,7 @@ class UserController extends Controller
                 $addUser->role_permission_id = $userPermission->id;
                 $addUser->branch_id = $request->branch_id == 'head_office' ? NULL : $request->branch_id;
             }
-        }else {
+        } else {
             $addUser->branch_id = $request->belonging_branch_id == 'head_office' ? NULL : $request->belonging_branch_id;
         }
 
@@ -236,14 +231,14 @@ class UserController extends Controller
     {
         $this->validate($request, [
             'first_name' => 'required',
-            'email' => 'required|unique:admin_and_users,email,'.$userId,
+            'email' => 'required|unique:admin_and_users,email,' . $userId,
         ]);
 
         $updateUser = AdminAndUser::where('id', $userId)->first();
 
         if (isset($request->allow_login)) {
             $this->validate($request, [
-                'username' => 'required|unique:admin_and_users,username,'.$userId,
+                'username' => 'required|unique:admin_and_users,username,' . $userId,
                 'password' => 'sometimes|confirmed',
             ]);
 
@@ -251,7 +246,7 @@ class UserController extends Controller
                 $this->validate($request, [
                     'password' => 'required|confirmed',
                 ]);
-            }else {
+            } else {
                 $this->validate($request, [
                     'password' => 'sometimes|confirmed',
                 ]);
@@ -266,7 +261,7 @@ class UserController extends Controller
                 'emp_id' => 'required',
                 'salary' => 'required',
                 'pay_type' => 'required',
-            ],[
+            ], [
                 'department_id.required' => 'Department field is required.',
                 'shift_id.required' => 'Shift field is required.',
                 'emp_id.required' => 'Employee ID field is required.',
@@ -301,7 +296,7 @@ class UserController extends Controller
                 $updateUser->role_permission_id = $userPermission->id;
                 $updateUser->branch_id = $request->branch_id == 'head_office' ? NULL : $request->branch_id;
             }
-        }else {
+        } else {
             $updateUser->branch_id = $request->belonging_branch_id == 'head_office' ? NULL : $request->belonging_branch_id;
         }
 
@@ -340,11 +335,11 @@ class UserController extends Controller
     // Delete user
     public function delete($userId)
     {
-        
+
         if (auth()->user()->permission->user['user_delete'] == '0') {
             abort(403, 'Access Forbidden.');
         }
-        
+
         $deleteUser = AdminAndUser::find($userId);
 
         if ($deleteUser->role_type == 1) {

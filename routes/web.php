0d79f12e302aa3f29d5817dd95b42e9ae7f96480
Harrison Roy <koalasoftsolution@gmail.com>
2022-02-05 13:52:58 +0600

    web.php

--- routes/web.php
+++ routes/web.php
@@ -881,8 +881,75 @@ Route::get('/test', function () {
     //     $p->is_last_created = 0;
     //     $p->save();
     // }
-    
+
     //return array_merge($arr1, $arr2, $arr3);
+
+    $supplier = Supplier::where('id', $supplierId)->first();
+    $totalSupplierPurchase = DB::table('purchases')
+        ->where('supplier_id', $supplierId)
+        ->select(DB::raw('sum(total_purchase_amount) as total_purchase'))
+        ->groupBy('supplier_id')->get();
+
+    $totalSupplierPayment = DB::table('supplier_payments')
+        ->where('supplier_id', $supplierId)
+        ->where('type', 1)
+        ->select(DB::raw('sum(paid_amount) as s_paid'))
+        ->groupBy('supplier_id')->get();
+
+    $totalPurchasePayment = DB::table('purchase_payments')
+        ->leftJoin('purchases', 'purchase_payments.purchase_id', 'purchases.id')
+        ->where('purchase_payments.supplier_payment_id', NULL)
+        ->where('purchase_payments.payment_type', 1)
+        ->where('purchases.supplier_id', $supplierId)
+        ->select(DB::raw('sum(paid_amount) as p_paid'))
+        ->groupBy('purchases.supplier_id')->get();
+
+    $totalInvoiceReturn = DB::table('purchase_returns')
+        ->join('purchases', 'purchase_returns.purchase_id', 'purchases.id')
+        ->where('purchases.supplier_id', $supplierId)
+        ->select(DB::raw('sum(total_return_amount) as total_inv_return_amt'))
+        ->groupBy('purchases.supplier_id')->get();
+
+    $totalSupplierReturn = DB::table('purchase_returns')
+        ->where('purchase_returns.purchase_id', NULL)
+        ->where('purchase_returns.supplier_id', $supplierId)
+        ->select(
+            DB::raw('sum(total_return_amount) as total_sup_return_amt')
+        )->groupBy('purchase_returns.supplier_id')->get();
+
+    $totalInvoiceReturnPayment = DB::table('purchase_payments') // Paid on purchase return invoice due.
+        ->join('purchases', 'purchase_payments.purchase_id', 'purchases.id')
+        ->where('purchase_payments.supplier_payment_id', NULL)
+        ->where('purchase_payments.payment_type', 2)
+        ->where('purchases.supplier_id', $supplierId)
+        ->select(DB::raw('sum(paid_amount) as total_inv_return_paid'))
+        ->groupBy('purchases.supplier_id')->get();
+
+    $totalSupplierReturnPayment = DB::table('supplier_payments') // Paid on Total supplier return due.
+        ->where('supplier_id', $supplierId)
+        ->where('type', 2)
+        ->select(DB::raw('sum(paid_amount) as sr_paid'))
+        ->groupBy('supplier_id')->get();
+
+    $__totalInvoiceReturnPayment = DB::table('purchase_payments') // Paid on supplier return invoice due.
+        ->where('purchase_payments.purchase_id', NULL)
+        ->where('purchase_payments.supplier_payment_id', NULL)
+        ->where('purchase_payments.payment_type', 2)
+        ->where('purchase_payments.supplier_id', $supplierId)
+        ->select(DB::raw('sum(paid_amount) as total_inv_return_paid'))
+        ->groupBy('purchase_payments.supplier_id')->get();
+
+    $totalPurchase = $totalSupplierPurchase->sum('total_purchase');
+    $totalPaid = $totalSupplierPayment->sum('s_paid') + $totalPurchasePayment->sum('p_paid');
+    $totalReturn = $totalInvoiceReturn->sum('total_inv_return_amt')
+        + $totalSupplierReturn->sum('total_sup_return_amt');
+
+    $totalReturnPaid = $totalInvoiceReturnPayment->sum('total_inv_return_paid')
+        + $totalSupplierReturnPayment->sum('sr_paid')
+        + $__totalInvoiceReturnPayment->sum('total_inv_return_paid');
+
+    $totalDue = ($totalPurchase + $supplier->opening_balance + $totalReturnPaid) - $totalPaid - $totalReturn;
+    $returnDue = $totalReturn - ($totalPurchase + $supplier->opening_balance - $totalPaid) - $totalReturnPaid;
 });
 
 // All authenticated routes

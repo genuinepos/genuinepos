d22f90f8c746e558373770a262896279025af45c
Harrison Roy <koalasoftsolution@gmail.com>
2022-03-13 16:18:40 +0600

    product\products\ajax_vew\partial\warehouse_stock_details.blade.php

--- resources/views/product/products/ajax_view/partials/warehouse_stock_details.blade.php
+++ resources/views/product/products/ajax_view/partials/warehouse_stock_details.blade.php
@@ -18,9 +18,9 @@
             @if ($row->variant_name)
                 <tr>
                     <td class="text-start">{{ $row->variant_code }}</td>
-                    <td class="text-start">{{ $product->name.'-'.$row->variant_name }}</td>
+                    <td class="text-start">{{ $product->name.'('.$row->variant_name.')' }}</td>
                     <td class="text-start">{{ $row->warehouse_name.'/'.$row->warehouse_code }}</td>
-                    <td class="text-start"><b>{{ $row->variant_quantity.'('.$product->unit->code_name.')' }}</b></td>
+                    <td class="text-start"><b>{{ $row->variant_quantity.'/'.$product->unit->code_name }}</b></td>
                     <td class="text-start">
                         @php
                             $currentStockValue = $row->variant_cost_with_tax * $row->variant_quantity;
@@ -37,7 +37,7 @@
                     <td class="text-start">{{ $product->product_code }}</td>
                     <td class="text-start">{{ $product->name }}</td>
                     <td class="text-start">{{ $row->warehouse_name.'/'.$row->warehouse_code }}</td>
-                    <td class="text-start"><b>{{ $row->product_quantity.'('.$product->unit->code_name.')' }}</b></td>
+                    <td class="text-start"><b>{{ $row->product_quantity.'/'.$product->unit->code_name }}</b></td>
                     <td class="text-start">
                         @php
                             $currentStockValue = $product->product_cost_with_tax * $row->product_quantity;

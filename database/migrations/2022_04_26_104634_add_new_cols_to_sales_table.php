f283353adc25a558f5a6f53556d584ad252a808b
Harrison Roy <koalasoftsolution@gmail.com>
2022-04-26 15:38:47 +0600

    2022_04_26_104634_add_new_cols_to_sales_table.php

--- database/migrations/2022_04_26_104634_add_new_cols_to_sales_table.php
+++ database/migrations/2022_04_26_104634_add_new_cols_to_sales_table.php
@@ -15,9 +15,10 @@ class AddNewColsToSalesTable extends Migration
     {
         Schema::table('sales', function (Blueprint $table) {
             $table->decimal('all_total_payable', 22, 2)->after('attachment')->default(0);
-            $table->decimal('all_total_payable', 22, 2)->after('attachment')->default(0);
-            $table->decimal('gross_pay', 22, 2)->after('all_total_payable')->default(0);
-            $table->decimal('previous_due', 22, 2)->after('gross_pay')->default(0);
+            $table->decimal('previous_due', 22, 2)->after('all_total_payable')->default(0);
+            $table->decimal('gross_pay', 22, 2)->after('previous_due')->default(0);
+            $table->decimal('previous_due_paid', 22, 2)->after('gross_pay')->default(0);
+            $table->decimal('customer_running_balance', 22, 2)->after('previous_due_paid')->default(0);
         });
     }
 

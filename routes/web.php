468d57e6ca953a4a894a0b0b5d9aecd0bd61feee
Harrison Roy <koalasoftsolution@gmail.com>
2022-09-09 14:20:19 +0600

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1074,7 +1074,7 @@ Route::get('/test', function () {
     // }
 
     $user_ip = getenv('REMOTE_ADDR');
-    $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
+    return $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
     $country = $geo["geoplugin_countryName"];
     $city = $geo["geoplugin_city"];
 });

b65cae5094416614aa32c0e51bfb2ac9ca2c659c
Sunwarul Islam <sanoarulslm@gmail.com>
2022-11-16 11:57:01 +0600

    email settings set on database instead of .env file set:env package and its functionalities removed.

--- resources/views/communication/email/settings/index.blade.php
+++ resources/views/communication/email/settings/index.blade.php
@@ -21,55 +21,50 @@
 
                         <form id="email_settings_form" class="setting_form p-3" action="{{ route('communication.email.settings.store') }}" method="post">
                             @csrf
-                            <div class="form-group">
-                                <div class="setting_form_heading">
-                                    <h6 class="text-primary">Email Settings</h6>
-                                </div>
-                            </div>
 
                             <div class="form-group row">
                                 <div class="col-md-4">
                                     <label><strong>MAIL MAILER : </strong></label>
                                     <input type="text" name="MAIL_MAILER" class="form-control es_input"
                                         placeholder="MAIL MAILER" autocomplete="off"
-                                        value="{{ env('MAIL_MAILER') }}">
+                                        value="{{ $emailSetting['MAIL_MAILER'] }}">
                                 </div>
 
                                 <div class="col-md-4">
                                     <label><strong>MAIL HOST :</strong></label>
                                     <input type="text" name="MAIL_HOST" class="form-control es_input"
                                         placeholder="MAIL HOST" autocomplete="off"
-                                        value="{{ env('MAIL_HOST') }}">
+                                        value="{{ $emailSetting['MAIL_HOST'] }}">
                                 </div>
 
                                 <div class="col-md-4">
                                     <label><strong>MAIL PORT :</strong></label>
                                     <input type="text" name="MAIL_PORT" class="form-control  es_input"
                                         placeholder="MAIL PORT" autocomplete="off"
-                                        value="{{ env('MAIL_PORT') }}">
+                                        value="{{ $emailSetting['MAIL_PORT'] }}">
                                 </div>
                             </div>
-                                
+
                             <div class="form-group row mt-1">
                                 <div class="col-md-4">
                                     <label><strong>MAIL_USERNAME :</strong></label>
                                     <input type="text" name="MAIL_USERNAME" class="form-control es_input"
                                         placeholder="MAIL USERNAME" autocomplete="off"
-                                        value="{{ env('MAIL_USERNAME') }}">
+                                        value="{{ $emailSetting['MAIL_USERNAME'] }}">
                                 </div>
 
                                 <div class="col-md-4">
                                     <label><strong>MAIL PASSWORD :</strong></label>
                                     <input type="text" name="MAIL_PASSWORD" class="form-control es_input"
                                         placeholder="MAIL PASSWORD" autocomplete="off"
-                                        value="{{ env('MAIL_PASSWORD') }}">
+                                        value="{{ $emailSetting['MAIL_PASSWORD'] }}">
                                 </div>
 
                                 <div class="col-md-4">
                                     <label><strong>MAIL ENCRYPTION :</strong></label>
                                     <input type="text" name="MAIL_ENCRYPTION" class="form-control  es_input"
                                         placeholder="MAIL ENCRYPTION" autocomplete="off"
-                                        value="{{ env('MAIL_ENCRYPTION') }}">
+                                        value="{{ $emailSetting['MAIL_ENCRYPTION'] }}">
                                 </div>
                             </div>
 
@@ -78,36 +73,38 @@
                                     <label><strong>MAIL FROM ADDRESS :</strong></label>
                                     <input type="text" name="MAIL_FROM_ADDRESS" class="form-control es_input"
                                         placeholder="MAIL FROM ADDRESS" autocomplete="off"
-                                        value="{{ env('MAIL_FROM_ADDRESS') }}">
+                                        value="{{ $emailSetting['MAIL_FROM_ADDRESS'] }}">
                                 </div>
 
                                 <div class="col-md-4">
                                     <label><strong>MAIL FROM NAME :</strong></label>
                                     <input type="text" name="MAIL_FROM_NAME" class="form-control es_input"
                                         placeholder="MAIL FROM NAME" autocomplete="off"
-                                        value="{{ env('MAIL_FROM_NAME') }}">
+                                        value="{{ $emailSetting['MAIL_FROM_NAME'] }}">
                                 </div>
 
                                 <div class="col-md-4 mt-1">
                                     <div class="row mt-4">
                                         <p class="checkbox_input_wrap">
                                             <input type="checkbox"
-                                                {{  env('MAIL_ACTIVE') == 'true' ? 'CHECKED' : '' }}
-                                                name="MAIL_ACTIVE" autocomplete="off"> &nbsp; <b>Is Active</b> 
+                                                {{  $emailSetting['MAIL_ACTIVE'] ? 'checked' : '' }}
+                                                name="MAIL_ACTIVE" autocomplete="off"> &nbsp; <b>@lang('menu.is_active')</b>
                                         </p>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="form-group row mt-1">
-                                
+
                             </div>
 
                             <div class="row mt-2">
-                                <div class="col-md-12 text-end">
-                                    <button type="button" class="btn loading_button d-none"><i
-                                        class="fas fa-spinner text-primary"></i><b> Loading...</b></button>
-                                    <button class="btn btn-sm btn-success submit_button float-end">Save Change</button>
+                                <div class="col-md-12 d-flex justify-content-end">
+                                    <div class="btn-box">
+                                        <button type="button" class="btn loading_button d-none"><i
+                                            class="fas fa-spinner text-white"></i></button>
+                                        <button class="btn w-auto btn-success submit_button float-end">@lang('menu.save_change')</button>
+                                    </div>
                                 </div>
                             </div>
                         </form>
@@ -117,7 +114,7 @@
         </div>
     </div>
 </div>
-   
+
 @endsection
 @push('scripts')
     <script>

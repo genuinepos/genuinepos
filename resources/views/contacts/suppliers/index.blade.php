ca4d2ca1ff354013a14da95f59513fe63eb0bff0
Sunwarul Islam (PC-2) <sanoarulslm@gmail.com>
2022-11-21 17:28:35 +0600

    Page layout fix

--- resources/views/contacts/suppliers/index.blade.php
+++ resources/views/contacts/suppliers/index.blade.php
@@ -253,10 +253,12 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-12">
-                                <button type="button" class="btn loading_button d-none"><i class="fas fa-spinner text-primary"></i><b> Loading...</b></button>
-                                <button type="submit" class="c-btn button-success me-0 float-end submit_button">Save</button>
-                                <button type="reset" data-bs-dismiss="modal" class="c-btn btn_orange float-end">Close</button>
+                            <div class="col-md-12 d-flex justify-content-end">
+                                <div class="btn-loading">
+                                    <button type="button" class="btn loading_button d-none"><i class="fas fa-spinner"></i><span> Loading...</span></button>
+                                    <button type="reset" data-bs-dismiss="modal" class="btn btn-sm btn-danger">Close</button>
+                                    <button type="submit" class="btn btn-sm btn-success submit_button">Save</button>
+                                </div>
                             </div>
                         </div>
                     </form>

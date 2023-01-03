decaf5470cc09edddc7c3831170febcf2ea6f14e
Zillur Rahman <reja6402@gmail.com>
2023-01-03 16:29:40 +0600

    email settings refactored

--- app/Models/GeneralSetting.php
+++ app/Models/GeneralSetting.php
@@ -42,23 +42,21 @@ class GeneralSetting extends Model
 
     public function scopeSms($query)
     {
-        return json_decode($query->first()->sms_setting, true);
+        return $this->sms_setting ?? [];
     }
 
     public function scopeEmail($query)
     {
-        $existing = $query->first()->email_setting;
-        $existing =  isset($existing) && !empty($existing) ? $existing : '{}';
-        return json_decode($existing, true);
+        return $this->email_setting ?? [];
     }
 
     public function scopeIsSmsActive($query)
     {
-        return json_decode($query->first()->sms_setting, true)['status'] ? true : false;
+        return ($this->sms_setting['status'] ?? false) ? true : false;
     }
 
     public function scopeIsEmailActive($query)
     {
-        return json_decode($query->first()->email_setting, true)['MAIL_ACTIVE'] ? true : false;
+        return ($this->email_setting['MAIL_ACTIVE'] ?? false) ? true : false;
     }
 }

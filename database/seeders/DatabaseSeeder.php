b09d3606919e6146d06a8107e92124728526dd2d
Abdur Rahman <mdabdurrahman542@gmail.com>
2023-01-05 12:55:03 +0600

    pos database seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,6 +33,11 @@ class DatabaseSeeder extends Seeder
             $this->call(PermissionSeeder::class);
             $mailSettings = '{"MAIL_MAILER":"smtp","MAIL_HOST":"smtp.gmail.com","MAIL_PORT":"587","MAIL_USERNAME":"s1@gmail.com","MAIL_PASSWORD":"speeddigit@54321","MAIL_ENCRYPTION":"tls","MAIL_FROM_ADDRESS":"s1@gmail.com","MAIL_FROM_NAME":"SpeedDigit","MAIL_ACTIVE":true}';
 
+            $posOldSetting = '{"is_disable_draft":0,"is_disable_quotation":0,"is_disable_challan":0,"is_disable_hold_invoice":0,"is_disable_multiple_pay":1,"is_show_recent_transactions":0,"is_disable_discount":0,"is_disable_order_tax":0,"is_show_credit_sale_button":1,"is_show_partial_sale_button":1,"is_enabled_draft":1,"is_enabled_quotation":1,"is_enabled_hold_invoice":1,"is_enabled_suspend":1,"is_enabled_discount":1}';
+
+            $posNewSetting = '{"is_enabled_multiple_pay":1,"is_enabled_draft":1,"is_enabled_quotation":1,"is_enabled_suspend":1,"is_enabled_discount":1,"is_enabled_order_tax":1,"is_show_recent_transactions":1,"is_enabled_credit_full_sale":1,"is_enabled_hold_invoice":1}';
+
+            
         } catch (Exception $e) {
             dd($e->getMessage());
         } finally {

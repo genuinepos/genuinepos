c6078d42c9b3712295e30e611e8da363be37e8d3
Harrison Roy <koalasoftsolution@gmail.com>
2023-01-08 16:49:49 +0600

    refactored

--- database/seeders/HorizontalToVerticalConversionSeeder.php
+++ database/seeders/HorizontalToVerticalConversionSeeder.php
@@ -15,7 +15,7 @@ class HorizontalToVerticalConversionSeeder extends Seeder
     public function run()
     {
         GeneralSetting::truncate();
-        $general_settings['business'] = '{"shop_name":"Genuine POS","address":"Uttara Sector 4, Dhaka","phone":"01970133444","email":"info@genuinepos.com","start_date":"07-04-2021","default_profit":0,"currency":"\u09f3","currency_placement":null,"date_format":"d-m-Y","financial_year_start":"January","time_format":"12","business_logo":"","timezone":"Asia\/Dhaka"}';
+        $general_settings['business'] = '{"shop_name":"GenuinePos","address":"Uttara, Sector - 4, Road No - 7, House No - 17, Dhaka, Bangladesh.","phone":"01719244933, 1722789897","email":"gollachuttelecare@gmail.com","start_date":null,"default_profit":0,"currency":"TK.","currency_placement":null,"date_format":"d-m-Y","stock_accounting_method":"2","time_format":"12","business_logo":"6352b2cbad890-.png","timezone":"Asia\/Dhaka"}';
 
         $general_settings['tax'] = '{"tax_1_name":"Tax","tax_1_no":"1","tax_2_name":"GST","tax_2_no":"2","is_tax_en_purchase_sale":1}';
         $general_settings['product'] = '{"product_code_prefix":null,"default_unit_id":"3","is_enable_brands":1,"is_enable_categories":1,"is_enable_sub_categories":1,"is_enable_price_tax":0,"is_enable_warranty":1}';
@@ -27,7 +27,7 @@ class HorizontalToVerticalConversionSeeder extends Seeder
         $general_settings['dashboard'] = '{"view_stock_expiry_alert_for":"31"}';
         $general_settings['system'] = '{"theme_color":"dark-theme","datatable_page_entry":"25"}';
 
-        $general_settings['prefix'] = '{"purchase_invoice":"PI","sale_invoice":"SI","purchase_return":"PRI","stock_transfer":"STI","stock_djustment":"SAR","sale_return":"SRI","expenses":"EXI","supplier_id":"SID","customer_id":null,"purchase_payment":"PPI","sale_payment":"SPI","expanse_payment":"EXPI"}';
+        $general_settings['prefix'] = '{"purchase_invoice":"PI","sale_invoice":"SI","purchase_return":"PRI","stock_transfer":"STI","stock_djustment":"SAR","sale_return":"SRI","expenses":"EXI","supplier_id":"SID","customer_id":null,"purchase_payment":"PV","sale_payment":"SPV","expanse_payment":"EPV"}';
 
         $general_settings['send_es_settings'] = '{"send_inv_via_email":0,"send_notice_via_sms":0,"cmr_due_rmdr_via_email":0,"cmr_due_rmdr_via_sms":0}';
 
@@ -38,7 +38,7 @@ class HorizontalToVerticalConversionSeeder extends Seeder
 
         $general_settings['reward_point_settings'] = '{"enable_cus_point":1,"point_display_name":"Reward Point","amount_for_unit_rp":"10","min_order_total_for_rp":"100","max_rp_per_order":"","redeem_amount_per_unit_rp":"0.10","min_order_total_for_redeem":"","min_redeem_point":"","max_redeem_point":""}';
 
-        $general_settings['mf_settings'] = '';
+        $general_settings['mf_settings'] = '{"production_ref_prefix":"MF","enable_editing_ingredient_qty":1,"enable_updating_product_price":1}';
         $general_settings['multi_branches'] = '0';
         $general_settings['hrm'] = '0';
         $general_settings['services'] = '0';

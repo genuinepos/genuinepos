994c90aff85beb327dd6d5cc42526b1700f97f27
Harrison Roy <koalasoftsolution@gmail.com>
2023-02-16 15:57:23 +0600

    Customer controller

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -146,8 +146,7 @@ class CustomerController extends Controller
 
         $this->userActivityLogUtil->addLog(action: 1, subject_type: 1, data_obj: $addCustomer);
 
-        // return $addCustomer;
-        return response()->json(['Customer created successfully!']);
+        return $addCustomer;
     }
 
     public function edit($customerId)

88387146942af5db5c6b536d07f456ddffab0c54
Sunwarul Islam <sanoarulslm@gmail.com>
2023-06-20 19:25:33 +0600

    vite setup. welcome page and register page HTML markup.

--- resources/views/components/saas-layout.blade.php
+++ resources/views/components/saas-layout.blade.php
@@ -1,3 +1,4 @@
+@props(['title' => null])
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +6,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ (isset($title) ? ($title . ' | ') : '') . config('app.name') }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+    @stack('css')
+    @vite(['resources/js/app.js'])
 </head>
 
 <body>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', fn() => view('saas.welcome-page'));
+Route::get('/saas/register', fn() => view('saas.register'))->name('saas.register');
 
 Auth::routes();
 Route::get('pin_login', fn () => view('auth.pin_login'));

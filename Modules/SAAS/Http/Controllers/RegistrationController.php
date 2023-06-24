1f0a700b4ff3a0e7968a9813213dca2392e24a67
Sunwarul Islam <sanoarulslm@gmail.com>
2023-06-24 11:49:22 +0600

    namespaces issue solved.

--- Modules/SAAS/Http/Controllers/RegistrationController.php
+++ Modules/SAAS/Http/Controllers/RegistrationController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\SAAS\Http\Controllers\SAAS;
+namespace Modules\SAAS\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
--- public/build/assets/app-9a77ba08.js
+++
@@ -1,1 +0,0 @@
-console.log("Resources JS Installed");

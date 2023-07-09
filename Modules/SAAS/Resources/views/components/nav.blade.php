350e8190dfa6693d7cdae249337d1d4a32be1cac
Sunwarul Islam <sanoarulslm@gmail.com>
2023-07-09 14:30:31 +0600

    nav and pint formats.

--- Modules/SAAS/Resources/views/components/nav.blade.php
+++ Modules/SAAS/Resources/views/components/nav.blade.php
@@ -1,61 +1,55 @@
-<nav class="navbar navbar-expand-lg @auth navbar-dark bg-secondary @else navbar-light bg-light @endauth">
+<nav class="navbar navbar-expand-lg px-5 @auth navbar-dark bg-secondary @else navbar-light bg-light @endauth">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ config('app.url') }}">{{ config('app.name') }}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="collapse navbar-collapse d-flex align-items-center justify-content-between" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0">
                 <li class="nav-item">
                     @auth
-                    <a class="nav-link active" aria-current="page" href="{{ route('saas.dashboard') }}">Dashboard</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('saas.dashboard') }}">{{ __('Dashboard') }}</a>
                     @else
-                    <a class="nav-link active" aria-current="page" href="{{ route('saas.welcome-page') }}">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('saas.welcome-page') }}">{{__('Home') }}</a>
                     @endauth
                 </li>
                 @auth
 
                 <li class="nav-item dropdown  float-end">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownShop" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Shops
+                        {{ __('Shops') }}
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownShop">
                         @can('tenants_index')
-                        <li><a class="dropdown-item" href="{{ route('saas.tenants.index') }}">All Shops</a></li>
+                        <li><a class="dropdown-item" href="{{ route('saas.tenants.index') }}">{{ __('All Shops') }}/a></li>
                         @endcan
                         @can('tenants_create')
-                        <li><a class="dropdown-item" href="{{ route('saas.tenants.create') }}">Create New</a></li>
+                        <li><a class="dropdown-item" href="{{ route('saas.tenants.create') }}">{{ __('Create New') }}</a></li>
                         @endcan
-                    </ul>
-                </li>
-                <li class="nav-item dropdown  float-end">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownSetting" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Profile
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownSetting">
-                        @can('profile_edit')
-                        <li><a class="dropdown-item" href="{{ route('saas.profile.edit', auth()->user()->id) }}">Edit Profile</a></li>
-                        @endcan
-                        <li><hr class="dropdown-divider"></li>
-                        <li  onclick="handleLogout()"><a class="dropdown-item" href="#" >Logout</a></li>
                     </ul>
                 </li>
                 @endauth
             </ul>
             <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-success" type="submit">Search</button>
+                <input class="form-control me-2" type="search" placeholder="{{ __('Search') }}" aria-label="Search">
+                <button class="btn btn-success" type="submit">{{ __('Search') }}</button>
             </form>
 
             <ul class="navbar-nav  mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('saas.dashboard') }}">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('saas.dashboard') }}">
-                        {{ auth()->user()->name }}
+                @auth
+                <li class="nav-item dropdown  float-end">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownSetting" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ auth()?->user()?->name }}
                     </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownSetting">
+                        @can('profile_edit')
+                        <li><a class="dropdown-item" href="{{ route('saas.profile.edit', auth()->user()->id) }}">{{ __('Edit Profile') }}</a></li>
+                        @endcan
+                        <li><hr class="dropdown-divider"></li>
+                        <li  onclick="handleLogout()"><a class="dropdown-item" href="#" >{{ __('Logout') }}</a></li>
+                    </ul>
                 </li>
+                @endauth
             </ul>
         </div>
     </div>

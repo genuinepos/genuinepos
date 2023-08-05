40ffe6a3d547984eecee7837c9241b5556eb9bc8
Sunwarul Islam <sanoarulslm@gmail.com>
2023-08-05 14:41:31 +0600

    init command

--- Modules/SAAS/Console/BackupCommand.php
+++ Modules/SAAS/Console/BackupCommand.php
@@ -3,8 +3,8 @@
 namespace Modules\SAAS\Console;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Artisan;
 use Modules\SAAS\Entities\Tenant;
+use Illuminate\Support\Facades\Artisan;
 
 class BackupCommand extends Command
 {
--- routes/console.php
+++ routes/console.php
@@ -1,1 +1,7 @@
 <?php
+
+use Illuminate\Support\Facades\Artisan;
+
+Artisan::command('dev:init', function () {
+    Artisan::call('db:seed --class=TenancyDatabaseSeeder');
+});

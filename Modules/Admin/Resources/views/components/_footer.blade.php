240acd9f44c6df14cfd47f3a60f8ddf7623f0f37
Sunwarul Islam <sanoarulslm@gmail.com>
2023-08-10 11:12:26 +0600

    Admin module removed. Only SAAS should be used -- BOSS

--- Modules/Admin/Resources/views/components/_footer.blade.php
+++
@@ -1,7 +0,0 @@
- <!-- footer start -->
- <div class="footer">
-     <p>Copyright &copy; {{ date('Y') }}
-         {{ __('All Rights Reserved By') }} <span class="text-primary">{{ config('app.name') }}</span>
-     </p>
- </div>
- <!-- footer end -->

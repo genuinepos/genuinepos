0ec1973c9617981fc84a1c73b99627a596e4dc89
Sunwarul Islam <sanoarulslm@gmail.com>
2023-09-14 11:58:18 +0600

    central domain from .env file.

--- config/tenancy.php
+++ config/tenancy.php
@@ -20,8 +20,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        // '127.0.0.1',
-        'pos.test',
+        env('CENTRAL_DOMAIN', '127.0.0.1'),
     ],
 
     /**

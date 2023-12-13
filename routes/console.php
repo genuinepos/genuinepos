902f1147bf2d85b7dc06849140d5567cfe102b48
Sunwarul Islam <sanoarulslm@gmail.com>
2023-12-13 18:43:38 +0600

    Parent shop > Shop > Business settings logic data, seeder, and test successfull. Made seeder drive data successfully.

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\GeneralSetting;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Artisan;
 use Database\Seeders\AccountGroupSeeder;
@@ -33,4 +35,44 @@ Artisan::command('dev:init', function () {
 
 Artisan::command('dev:seed', function () {
     (new AccountGroupSeeder)->run();
+});
+
+Artisan::command('t:1', function () {
+    $s = GeneralSetting::where('parent_branch_id', auth()->user()?->branch_id)
+        ->orWhere('branch_id', auth()->user()?->branch_id)
+        ->orWhereNull('branch_id')
+        ->distinct('key')
+        ->pluck('value', 'key')
+        ->toArray();
+    dd($s);
+});
+
+Artisan::command('t:2', function () {
+
+    $generalSettings = GeneralSetting::where(function ($query) {
+        $query->whereNotNull('branch_id')
+            ->whereNotNull('parent_branch_id')
+            ->orderBy('branch_id')
+            ->orderBy('parent_branch_id');
+        })
+        ->orWhere(function ($query) {
+            $query->whereNotNull('branch_id')
+                ->whereNull('parent_branch_id')
+                ->orderBy('branch_id');
+        })
+        ->orWhere(function ($query) {
+            $query->whereNull('branch_id')
+                ->whereNotNull('parent_branch_id')
+                ->orderBy('parent_branch_id');
+        })
+        ->orWhere(function ($query) {
+            $query->whereNull('branch_id')
+                ->whereNull('parent_branch_id')
+                ->orderBy(DB::raw('RAND()')); // Random order for global settings
+        })
+        ->distinct('key')
+        ->pluck('value', 'key');
+    // ->get();
+
+    dd($generalSettings);
 });

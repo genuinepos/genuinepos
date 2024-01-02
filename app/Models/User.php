954477de503bfea1c3441f0ec70a39a00c852d57
Harrison Roy <koalasoftsolution@gmail.com>
2024-01-02 19:20:25 +0600

    User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-partition
+
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, HasRoles, Notifiable;

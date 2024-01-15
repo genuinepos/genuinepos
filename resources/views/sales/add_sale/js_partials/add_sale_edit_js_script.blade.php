78c0df928d9b2d93ec25fda6e31abe142e77c6f5
Harrison Roy <koalasoftsolution@gmail.com>
2024-01-15 12:17:46 +0600

    add sale edit js

--- resources/views/sales/add_sale/js_partials/add_sale_edit_js_script.blade.php
+++ resources/views/sales/add_sale/js_partials/add_sale_edit_js_script.blade.php
@@ -296,7 +296,6 @@
     }
 
     var keyName = 1;
-
     function selectProduct(e) {
 
         var price_group_id = $('#price_group_id').val() ? $('#price_group_id').val() : 'no_id';
@@ -463,13 +462,13 @@
 
         if (e_quantity == '') {
 
-            toastr.error('Quantity field must not be empty.');
+            toastr.error("{{ __('Quantity field must not be empty.') }}");
             return;
         }
 
         if (e_product_id == '') {
 
-            toastr.error('Please select an item.');
+            toastr.error("{{ __('Please select an item.') }}");
             return;
         }
 
@@ -499,16 +498,21 @@
 
                     var check_quantity = parseFloat(e_quantity);
 
-                    if (e_current_warehouse_id == e_warehouse_id) {
+                    if (e_warehouse_id != '' && e_current_warehouse_id == e_warehouse_id) {
 
                         check_quantity = parseFloat(e_quantity) - parseFloat(e_current_quantity);
                     }
 
-                    var stockLocationMessage = e_warehouse_id ? ' in selected warehouse' : ' in the Shop/Business';
+                    if (e_warehouse_id == '') {
+
+                        check_quantity = parseFloat(e_quantity) - parseFloat(e_current_quantity);
+                    }
+
+                    var stockLocationMessage = e_warehouse_id ? " {{ __('in selected warehouse') }}" : " {{ __('in the Shop/Business') }}";
 
                     if (parseFloat(check_quantity) > parseFloat(data.stock)) {
 
-                        toastr.error('Current stock is ' + parseFloat(data.stock) + stockLocationMessage);
+                        toastr.error("{{ __('Current stock is') }} " + parseFloat(data.stock) + stockLocationMessage);
                         return;
                     }
 
@@ -627,6 +631,7 @@
         var tr = $(this);
         var unique_id = tr.find('#unique_id').val();
         var warehouse_id = tr.find('#warehouse_id').val();
+        var current_quantity = tr.find('#current_quantity').val();
         var current_warehouse_id = tr.find('#current_warehouse_id').val();
         var stock_location_name = tr.find('#stock_location_name').html();
         var item_name = tr.find('#item_name').val();
@@ -672,6 +677,7 @@
         $('#search_product').val(item_name);
         $('#e_unique_id').val(unique_id);
         $('#e_warehouse_id').val(warehouse_id);
+        $('#e_current_quantity').val(current_quantity);
         $('#e_current_warehouse_id').val(current_warehouse_id);
         $('#e_stock_location_name').val(stock_location_name);
         $('#e_item_name').val(item_name);

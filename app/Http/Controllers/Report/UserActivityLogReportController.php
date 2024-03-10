ddfef6a32c7a497352be7608ddc9a47391f812e1
Harrison Roy <koalasoftsolution@gmail.com>
2024-03-10 15:56:55 +0600

    UserActivityLogReportController.php

--- app/Http/Controllers/Report/UserActivityLogReportController.php
+++ app/Http/Controllers/Report/UserActivityLogReportController.php
@@ -112,9 +112,17 @@ class UserActivityLogReportController extends Controller
             ->orderByRaw('COALESCE(branches.parent_branch_id, branches.id), branches.id')->get();
 
         $users = null;
-        if (auth()->user()->can('has_access_to_all_area') && config('generalSettings')['subscription']->current_shop_count > 0) {
+        if (
+            auth()->user()->can('has_access_to_all_area') &&
+            config('generalSettings')['subscription']->has_business == BooleanType::True->value &&
+            auth()->user()->is_belonging_an_area == BooleanType::False->value &&
+            !auth()->user()->can('user_activities_log_only_own_log')
+        ) {
 
             $users = $this->userService->users()->select('id', 'prefix', 'name', 'last_name')->get();
+        }else if(!auth()->user()->can('user_activities_log_only_own_log')) {
+
+            $users = $this->userService->users()->where('branch_id', auth()->user()->branch_id)->select('id', 'prefix', 'name', 'last_name')->get();
         }
 
         return view('reports.user_activity_log.index', compact('branches', 'users'));

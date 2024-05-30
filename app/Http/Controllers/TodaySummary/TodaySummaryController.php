18f9bb7f488933ee1719b63fd5eb2d27d5947598
Harrison Roy <koalasoftsolution@gmail.com>
2024-05-30 19:12:47 +0600

    fixed some issues

--- app/Http/Controllers/TodaySummary/TodaySummaryController.php
+++ app/Http/Controllers/TodaySummary/TodaySummaryController.php
@@ -2,23 +2,16 @@
 
 namespace App\Http\Controllers\TodaySummary;
 
-use App\Enums\RoleType;
-use App\Enums\SaleStatus;
-use App\Enums\BooleanType;
 use Illuminate\Http\Request;
-use App\Enums\PurchaseStatus;
 use Illuminate\Support\Facades\DB;
-use App\Enums\AccountingVoucherType;
 use App\Http\Controllers\Controller;
 use App\Services\Setups\BranchService;
 use App\Services\TodaySummary\TodaySummaryService;
 
 class TodaySummaryController extends Controller
 {
-    public function __construct(
-        private TodaySummaryService $todaySummaryService,
-        private BranchService $branchService,
-    ) {
+    public function __construct(private TodaySummaryService $todaySummaryService, private BranchService $branchService)
+    {
     }
 
     public function index(Request $request)

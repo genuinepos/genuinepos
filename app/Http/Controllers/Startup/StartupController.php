e9e190a0602df6583c3bba55051caf26c612eb40
Harrison Roy <koalasoftsolution@gmail.com>
2024-07-03 18:55:22 +0600

    refactored

--- app/Http/Controllers/Startup/StartupController.php
+++ app/Http/Controllers/Startup/StartupController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Startup;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Startup\StartupFinishRequest;

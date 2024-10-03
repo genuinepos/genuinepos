01affb420b08b6dc9abfaf8ac1397b079064c16e
Harrison Roy <koalasoftsolution@gmail.com>
2024-10-03 19:50:10 +0600

    feature update

--- resources/views/sales/order_to_invoice/create.blade.php
+++ resources/views/sales/order_to_invoice/create.blade.php
@@ -265,8 +265,7 @@
                                                             $brand = $orderedProduct?->product?->brand ? ' | ' . $orderedProduct?->product?->brand?->name : '';
                                                         @endphp
 
-                                                        <option value="{{ $orderedProduct->id }}" data-name="{{ $orderedProduct?->product?->name . ($orderedProduct?->variant ? '-' . $orderedProduct?->variant?->name : '') . ' (' . $orderedProduct?->product?->product_code . ')' }}" data-p_id="{{ $orderedProduct->product_id }}" data-is_manage_stock="{{ $orderedProduct?->product?->is_manage_stock }}" data-v_id="{{ $orderedProduct?->variant_id }}" data-p_tax_ac_id="{{ $orderedProduct->tax_ac_id ? $orderedProduct->tax_ac_id : '' }}" data-tax_type="{{ $orderedProduct?->tax_type }}" data-is_show_emi_on_pos="{{ $orderedProduct?->product?->is_show_emi_on_pos }}" data-p_price_exc_tax="{{ $orderedProduct?->unit_price_exc_tax }}" data-p_discount="{{ $orderedProduct?->unit_discount }}" data-p_discount_type="{{ $orderedProduct?->unit_discount_type }}" data-p_discount_amount="{{ $orderedProduct?->unit_discount_amount }}" data-p_price_inc_tax="{{ $orderedProduct?->unit_price_inc_tax }}"
-                                                            data-p_cost_inc_tax="{{ $orderedProduct?->unit_cost_inc_tax }}" data-p_ordered_quantity="{{ $orderedProduct?->ordered_quantity }}" data-p_delivered_quantity="{{ $orderedProduct?->delivered_quantity }}" data-p_left_quantity="{{ $orderedProduct?->left_quantity }}">
+                                                        <option value="{{ $orderedProduct->id }}" data-name="{{ $orderedProduct?->product?->name . ($orderedProduct?->variant ? '-' . $orderedProduct?->variant?->name : '') . ' (' . $orderedProduct?->product?->product_code . ')' }}" data-p_id="{{ $orderedProduct->product_id }}" data-is_manage_stock="{{ $orderedProduct?->product?->is_manage_stock }}" data-v_id="{{ $orderedProduct?->variant_id }}" data-p_tax_ac_id="{{ $orderedProduct->tax_ac_id ? $orderedProduct->tax_ac_id : '' }}" data-tax_type="{{ $orderedProduct?->tax_type }}" data-is_show_emi_on_pos="{{ $orderedProduct?->product?->is_show_emi_on_pos }}" data-p_price_exc_tax="{{ $orderedProduct?->unit_price_exc_tax }}" data-p_discount="{{ $orderedProduct?->unit_discount }}" data-p_discount_type="{{ $orderedProduct?->unit_discount_type }}" data-p_discount_amount="{{ $orderedProduct?->unit_discount_amount }}" data-p_price_inc_tax="{{ $orderedProduct?->unit_price_inc_tax }}" data-p_cost_inc_tax="{{ $orderedProduct?->unit_cost_inc_tax }}" data-p_ordered_quantity="{{ $orderedProduct?->ordered_quantity }}" data-p_delivered_quantity="{{ $orderedProduct?->delivered_quantity }}" data-p_left_quantity="{{ $orderedProduct?->left_quantity }}">
                                                             {{ $orderedProduct?->product?->name . ($orderedProduct?->variant ? '-' . $orderedProduct?->variant?->name : '') . ' (' . $orderedProduct?->product?->product_code . $brand . ')' }}
                                                         </option>
                                                     @endforeach
@@ -422,7 +421,7 @@
                                                 <div class="input-group">
                                                     <label class="col-4"><b>{{ __('Ship. Details') }} </b></label>
                                                     <div class="col-8">
-                                                        <input type="text" name="shipment_details" class="form-control" id="shipment_details" data-next="shipment_address" placeholder="{{ __('Shipment Details') }}">
+                                                        <input type="text" name="shipment_details" class="form-control" id="shipment_details" data-next="shipment_address" value="{{ $order?->shipment_details }}" placeholder="{{ __('Shipment Details') }}">
                                                     </div>
                                                 </div>
                                             </div>
@@ -431,7 +430,7 @@
                                                 <div class="input-group">
                                                     <label class="col-4"><b>{{ __('Ship. Address') }} </b></label>
                                                     <div class="col-8">
-                                                        <input name="shipment_address" type="text" class="form-control" id="shipment_address" data-next="shipment_status" placeholder="{{ __('Shipment Address') }}">
+                                                        <input name="shipment_address" type="text" class="form-control" id="shipment_address" data-next="shipment_status" value="{{ $order?->shipment_address }}" placeholder="{{ __('Shipment Address') }}">
                                                     </div>
                                                 </div>
                                             </div>
@@ -443,7 +442,7 @@
                                                         <select name="shipment_status" class="form-control" id="shipment_status" data-next="delivered_to">
                                                             <option value="">{{ __('Shipment Status') }}</option>
                                                             @foreach (\App\Enums\ShipmentStatus::cases() as $shipmentStatus)
-                                                                <option value="{{ $shipmentStatus->value }}">{{ $shipmentStatus->name }}</option>
+                                                                <option @selected($order?->shipment_status == $shipmentStatus->value) value="{{ $shipmentStatus->value }}">{{ $shipmentStatus->name }}</option>
                                                             @endforeach
                                                         </select>
                                                     </div>
@@ -454,7 +453,7 @@
                                                 <div class="input-group">
                                                     <label class="col-4"><b>{{ __('Delivered To') }} </b></label>
                                                     <div class="col-8">
-                                                        <input name="delivered_to" type="text" class="form-control" id="delivered_to" data-next="note" placeholder="{{ __('Delivered To') }}">
+                                                        <input type="text" name="delivered_to" class="form-control" id="delivered_to" value="{{ $order?->delivered_to }}" data-next="note" placeholder="{{ __('Delivered To') }}">
                                                     </div>
                                                 </div>
                                             </div>

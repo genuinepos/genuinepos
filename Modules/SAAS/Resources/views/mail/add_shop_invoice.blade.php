f2faea6ae4245c367acb0dfb949e10e874158e4d
Harrison Roy <koalasoftsolution@gmail.com>
2024-10-06 15:19:40 +0600

    test

--- Modules/SAAS/Resources/views/mail/add_shop_invoice.blade.php
+++ Modules/SAAS/Resources/views/mail/add_shop_invoice.blade.php
@@ -405,6 +405,7 @@
                         <div class="mail-header">
                             <div class="logo">
                                 <img style="height: height; width:auto;" src="{{ config('app.url') }}/modules/saas/images/logo_black.png" alt="System Logo" class="logo__img">
+                                {{ config('app.url') }}/modules/saas/images/logo_black.png
                             </div>
                             <div class="content_customer" style="text-align: right;">
                                 <span style="font-weight: bold;">{{ config('speeddigit.name') }}</span> <br>

11389b5b1232e8719c9ae4428138953f7aeabf3d
Harrison Roy <koalasoftsolution@gmail.com>
2024-10-21 19:01:37 +0600

    test

--- public/assets/plugins/custom/install-pwa-app.js
+++ public/assets/plugins/custom/install-pwa-app.js
@@ -1,18 +1,18 @@
-if ("serviceWorker" in navigator) {
-    // Register a service worker hosted at the root of the
-    // site using the default scope.
-    navigator.serviceWorker.register("/sw.js").then(
-        (registration) => {
+// if ("serviceWorker" in navigator) {
+//     // Register a service worker hosted at the root of the
+//     // site using the default scope.
+//     navigator.serviceWorker.register("/sw.js").then(
+//         (registration) => {
 
-            // console.log("Service worker registration succeeded:", registration);
-        },
-        (error) => {
-            console.error(`Service worker registration failed: ${error}`);
-        },
-    );
-} else {
-    console.error("Service workers are not supported.");
-}
+//             // console.log("Service worker registration succeeded:", registration);
+//         },
+//         (error) => {
+//             console.error(`Service worker registration failed: ${error}`);
+//         },
+//     );
+// } else {
+//     console.log("Service workers are not supported.");
+// }
 
 if ('serviceWorker' in navigator && 'BeforeInstallPromptEvent' in window) {
     // Listen for the beforeinstallprompt event

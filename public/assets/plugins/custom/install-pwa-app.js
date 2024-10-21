d96d4d228964ab4f180e221c6c903bba75352dbd
Harrison Roy <koalasoftsolution@gmail.com>
2024-10-21 18:54:04 +0600

    test

--- public/assets/plugins/custom/install-pwa-app.js
+++ public/assets/plugins/custom/install-pwa-app.js
@@ -2,13 +2,13 @@ if ("serviceWorker" in navigator) {
     // Register a service worker hosted at the root of the
     // site using the default scope.
     navigator.serviceWorker.register("/sw.js").then(
-    (registration) => {
+        (registration) => {
 
-        // console.log("Service worker registration succeeded:", registration);
-    },
-    (error) => {
-        console.error(`Service worker registration failed: ${error}`);
-    },
+            // console.log("Service worker registration succeeded:", registration);
+        },
+        (error) => {
+            console.error(`Service worker registration failed: ${error}`);
+        },
     );
 } else {
     console.error("Service workers are not supported.");
@@ -17,61 +17,61 @@ if ("serviceWorker" in navigator) {
 if ('serviceWorker' in navigator && 'BeforeInstallPromptEvent' in window) {
     // Listen for the beforeinstallprompt event
     window.addEventListener('beforeinstallprompt', (event) => {
-    // Prevent the default "Add to Home Screen" prompt
-    event.preventDefault();
+        // Prevent the default "Add to Home Screen" prompt
+        event.preventDefault();
 
-    window.addEventListener('DOMContentLoaded', () => {
-        let displayMode = 'browser tab';
-        if (window.matchMedia('(display-mode: standalone)').matches) {
-          displayMode = 'standalone';
-        }
-        // Log launch display mode to analytics
-        console.log('DISPLAY_MODE_LAUNCH:', displayMode);
-      });
-
-    // Show the "Install App" button
-    let installButton = document.getElementById('appInstallModal');
-    let closeInstallModal = document.getElementById('closeInstallModal');
-    let pwaInstallModal;
-    if (installButton) {
-        pwaInstallModal = new bootstrap.Modal(installButton, {
-            keyboard: true,
-            backdrop: true,
+        window.addEventListener('DOMContentLoaded', () => {
+            let displayMode = 'browser tab';
+            if (window.matchMedia('(display-mode: standalone)').matches) {
+                displayMode = 'standalone';
+            }
+            // Log launch display mode to analytics
+            console.log('DISPLAY_MODE_LAUNCH:', displayMode);
         });
 
-        pwaInstallModal.show();
+        // Show the "Install App" button
+        let installButton = document.getElementById('appInstallModal');
+        let closeInstallModal = document.getElementById('closeInstallModal');
+        let pwaInstallModal;
+        if (installButton) {
+            pwaInstallModal = new bootstrap.Modal(installButton, {
+                keyboard: true,
+                backdrop: true,
+            });
 
-        if (closeInstallModal) {
-            closeInstallModal.addEventListener('click', () => {
+            pwaInstallModal.show();
+
+            if (closeInstallModal) {
+                closeInstallModal.addEventListener('click', () => {
+                    pwaInstallModal.hide();
+                });
+            }
+        }
+
+        const installPwa = document.getElementById('installPwa');
+
+        if (installPwa) {
+            installPwa.addEventListener('click', () => {
                 pwaInstallModal.hide();
             });
         }
-    }
 
-    const installPwa = document.getElementById('installPwa');
+        // Save the event for later use
+        let deferredPrompt = event;
 
-    if(installPwa) {
-        installPwa.addEventListener('click', () => {
-            pwaInstallModal.hide();
-        });
-    }
-
-    // Save the event for later use
-    let deferredPrompt = event;
-
-    // Add event listener to the "Install App" button
+        // Add event listener to the "Install App" button
         installPwa.addEventListener('click', () => {
             // Trigger the "Add to Home Screen" prompt
             deferredPrompt.prompt();
 
             // Wait for the user to respond to the prompt
             deferredPrompt.userChoice
-            .then((choiceResult) => {
-                // Reset the prompt variable
-                deferredPrompt = null;
-                // Hide the "Install App" button after the prompt is shown
-                pwaInstallModal.hide();
-            });
+                .then((choiceResult) => {
+                    // Reset the prompt variable
+                    deferredPrompt = null;
+                    // Hide the "Install App" button after the prompt is shown
+                    pwaInstallModal.hide();
+                });
         });
     });
 }
